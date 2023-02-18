9c95d87a52d3de88767d8a570a0f174926fee26f
ikeyan <abetyan31503@gmail.com>
2023-02-18 18:11:41 +0900

    Modify: リファクタリングを実施

--- server_test.go
+++ server_test.go
@@ -11,17 +11,20 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-func TestRun(t *testing.T){
-	t.Skip("リファクタリング中")
-
+func TestServer_Run(t *testing.T) {
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("failed to listen port %v", err)
 	}
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	})
+
 	eg.Go(func() error {
-		return run(ctx,)
+		s := NewServer(l, mux)
+		return s.Run(ctx)
 	})
 	in := "message"
 	url := fmt.Sprintf("http://%s/%s", l.Addr().String(), in)
@@ -41,8 +44,7 @@ func TestRun(t *testing.T){
 	if string(got) != want {
 		t.Errorf("want %q, but got %q", want, got)
 	}
-
-	// run関数に終了通知を送信する
+	// run関数に終了通知を送信する。
 	cancel()
 	// run関数の戻り値を検証する
 	if err := eg.Wait(); err != nil {
